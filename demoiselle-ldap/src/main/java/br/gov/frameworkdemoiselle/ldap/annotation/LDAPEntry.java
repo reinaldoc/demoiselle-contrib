14f802f993395f270af0f4bf51aaeb9edb7406a7
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-11 16:01:47 -0300

    annotations para tratamento dos POJOs

---
+++ demoiselle-ldap/src/main/java/br/gov/frameworkdemoiselle/ldap/annotation/LDAPEntry.java
@@ -0,0 +1,12 @@
+package br.gov.frameworkdemoiselle.ldap.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface LDAPEntry {
+}
