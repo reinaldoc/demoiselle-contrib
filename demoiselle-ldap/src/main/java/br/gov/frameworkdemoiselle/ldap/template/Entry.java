488f17e3414b29284dd6c55fe832db880c509797
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-22 19:53:19 -0300

    Classe que prover atributos comuns a todos do domains @LDAPEntry. O construtor Entry(boolean) deve ser chamado quando for necessário que a nova instância não tenha o atributo objectClass definido.

--- demoiselle-ldap/src/main/java/br/gov/frameworkdemoiselle/ldap/template/Entry.java
+++ demoiselle-ldap/src/main/java/br/gov/frameworkdemoiselle/ldap/template/Entry.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2012 - Reinaldo de Carvalho <reinaldoc@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. 
+ * 
+ */
+
 package br.gov.frameworkdemoiselle.ldap.template;
 
 import br.gov.frameworkdemoiselle.ldap.annotation.DistinguishedName;
@@ -17,8 +32,8 @@ public abstract class Entry {
 		setObjectClass(objectClass());
 	}
 
-	public Entry(boolean setObjectClass) {
-		if (setObjectClass)
+	public Entry(boolean skipObjectClass) {
+		if (!skipObjectClass)
 			setObjectClass(objectClass());
 	}
 
