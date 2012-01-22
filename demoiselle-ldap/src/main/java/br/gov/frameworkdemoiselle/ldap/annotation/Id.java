925ab8599cfb1e1a9d49095493a48d425c555f5e
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-22 19:46:19 -0300

    Anotações para o uso de entidades;
    @LDAPEntry - anotação de classe que identifica o domain que representa
    resgistros LDAP que contém o objectClass com mesmo nome da classe.
    
    @DistinguishedName - anotação de atributo que armazenará o DN do
    registro. É obrigatória.
    
    @Id - Identifica o campo que terá o seu valor usado no relacionamento
    entre classes @LDAPEntry. Uma classe "X" anotada com @LDAPEntry pode
    conter atributos do tipo de outra classe "Y" @LDAPEntry, assim quando a
    classe "X" for persisida, o campo anotado com @Id da classe "Y" será
    gravado na classe "X". O contrário quando o registro for carregado.
    
    A persistencia não implementa Cascade, assim, somente o registro que
    esta sendo persistido será comitado ao LDAP.

--- demoiselle-ldap/src/main/java/br/gov/frameworkdemoiselle/ldap/annotation/Id.java
+++ demoiselle-ldap/src/main/java/br/gov/frameworkdemoiselle/ldap/annotation/Id.java
@@ -15,13 +15,13 @@
 
 package br.gov.frameworkdemoiselle.ldap.annotation;
 
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Target(TYPE)
+@Target(FIELD)
 @Retention(RUNTIME)
-public @interface LDAPEntry {
+public @interface Id {
 }
