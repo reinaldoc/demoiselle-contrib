886418baab1b1d8a02d59fa667594e9fa797e48a
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-24 22:00:50 -0300

    novo parâmetro:
    fuselage.authenticators.modules - lista de modulos de autent. ativados

---
+++ demoiselle-fuselage/src/main/java/br/gov/frameworkdemoiselle/fuselage/configuration/AuthenticatorConfig.java
@@ -0,0 +1,26 @@
+package br.gov.frameworkdemoiselle.fuselage.configuration;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.gov.frameworkdemoiselle.annotation.Name;
+import br.gov.frameworkdemoiselle.configuration.Configuration;
+
+@Configuration(resource = "fuselage", prefix = "fuselage.authenticators")
+public class AuthenticatorConfig implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Name("modules")
+	private List<String> authenticators = new ArrayList<String>();
+
+	public List<String> getAuthenticators() {
+		return authenticators;
+	}
+
+	public void setAuthenticators(List<String> authenticators) {
+		this.authenticators = authenticators;
+	}
+
+}
