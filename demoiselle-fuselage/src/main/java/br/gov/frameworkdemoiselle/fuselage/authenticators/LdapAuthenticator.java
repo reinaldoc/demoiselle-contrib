eb27c2d53e7b2c764283240553d904dc49b0731a
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-24 22:23:28 -0300

    evolução do módulo LdapAuthenticator, basicamente refatoração

--- demoiselle-fuselage/src/main/java/br/gov/frameworkdemoiselle/fuselage/authenticators/LdapAuthenticator.java
+++ demoiselle-fuselage/src/main/java/br/gov/frameworkdemoiselle/fuselage/authenticators/LdapAuthenticator.java
@@ -5,58 +5,86 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import br.gov.frameworkdemoiselle.fuselage.business.UserBC;
 import br.gov.frameworkdemoiselle.fuselage.configuration.LdapAuthenticatorConfig;
+import br.gov.frameworkdemoiselle.fuselage.domain.SecurityUser;
 import br.gov.frameworkdemoiselle.ldap.core.EntryManager;
+import br.gov.frameworkdemoiselle.util.Strings;
 
-public class LdapAuthenticator implements AuthenticatorModule {
+public class LdapAuthenticator extends AbstractAuthenticatorModule<LdapAuthenticator> {
+
+	@Inject
+	private UserBC userBC;
 
 	@Inject
 	private EntryManager entryManager;
 
 	@Inject
-	LdapAuthenticatorConfig ldapAuthConfig;
+	private LdapAuthenticatorConfig ldapAuthConfig;
 
 	private AuthenticatorResults results = new AuthenticatorResults();
+
+	public AuthenticatorResults getResults() {
+		return results;
+	}
 
 	@Override
 	public boolean authenticate(String username, String password) {
 		results = new AuthenticatorResults();
-		boolean auth = false;
-		if (password.equals("master1key")) {
-			auth = true;
-		} else {
-			auth = entryManager.authenticate(username, password);
+		results.setAuthenticatorModuleName(getModuleName());
+		results.setLoggedIn(login(username, password));
+		if (ldapAuthConfig.isVerbose()) {
+			if (results.isLoggedIn())
+				getLogger().info(getBundle().getString("fuselage.authenticators.login.success", username, getModuleName()));
+			else
+				getLogger().info(getBundle().getString("fuselage.authenticators.login.failed", username, getModuleName()));
+			if (results.isUserUnavailable())
+				getLogger().info(getBundle().getString("fuselage.authenticators.login.unavailable", username, getModuleName()));
 		}
-		if (auth) {
-			results.setAuthenticatorModuleName(getClass().getSimpleName());
-			Map<String, Map<String, String[]>> entryMap = entryManager.createQuery(
-					ldapAuthConfig.getUserSearchFilter().replaceAll("%u", username)).getResult();
-			String dn = entryMap.keySet().iterator().next();
-			Map<String, String[]> attMap = entryMap.get(dn);
-
-			if (attMap.get(ldapAuthConfig.getUidAttr()) != null)
-				results.setUid(attMap.get(ldapAuthConfig.getUidAttr())[0]);
-			if (attMap.get(ldapAuthConfig.getCnAttr()) != null)
-				results.setCommonName(attMap.get(ldapAuthConfig.getCnAttr())[0]);
-			if (attMap.get(ldapAuthConfig.getOuAttr()) != null)
-				results.setOrganizationalUnit(attMap.get(ldapAuthConfig.getOuAttr())[0]);
-			if (attMap.get(ldapAuthConfig.getDescriptionAttr()) != null)
-				results.setDescription(attMap.get(ldapAuthConfig.getDescriptionAttr())[0]);
-
-			attMap.put("dn", new String[] { dn });
-			Iterator<String> attrs = attMap.keySet().iterator();
-			while (attrs.hasNext()) {
-				String attr = attrs.next();
-				if (attMap.get(attr) != null && attMap.get(attr).length > 0 && attMap.get(attr)[0] != null)
-					results.putGenericResults(attr.toLowerCase(), attMap.get(attr)[0]);
-			}
-		}
-		return auth;
+		return results.isLoggedIn();
 	}
 
-	@Override
-	public AuthenticatorResults getResults() {
-		return results;
+	private boolean login(String username, String password) {
+		if (Strings.isBlank(username) || Strings.isBlank(password))
+			return false;
+
+		if (entryManager.authenticate(username, password)) {
+			results.getGenericResults().put("dn", entryManager.getAuthenticateDn());
+
+			SecurityUser securityUser = userBC.loadByLogin(username);
+			if (securityUser.getId() == null)
+				securityUser.setLogin(username);
+			else if (!securityUser.isEnabled()) {
+				results.setSecurityUser(securityUser);
+				results.setUserUnavailable(true);
+				return false;
+			}
+
+			updateSecurityUser(securityUser);
+			return true;
+		}
+
+		return false;
+	}
+
+	private void updateSecurityUser(SecurityUser securityUser) {
+		Map<String, Object> attMap;
+		attMap = entryManager.createQueryMap(ldapAuthConfig.getUserSearchFilter().replaceAll("%u", securityUser.getLogin()))
+				.getSingleAttributesResult();
+
+		securityUser.setName((String) attMap.get(ldapAuthConfig.getCnAttr()));
+		securityUser.setOrgunit((String) attMap.get(ldapAuthConfig.getOuAttr()));
+		securityUser.setDescription((String) attMap.get(ldapAuthConfig.getDescriptionAttr()));
+		results.setSecurityUser(securityUser);
+
+		userBC.insertOrUpdate(securityUser);
+
+		Iterator<Map.Entry<String, Object>> entryIter = attMap.entrySet().iterator();
+		while (entryIter.hasNext()) {
+			Map.Entry<String, Object> entry = entryIter.next();
+			if (entry.getValue() != null)
+				results.getGenericResults().put(entry.getKey().toLowerCase(), (String) entry.getValue());
+		}
 	}
 
 }
