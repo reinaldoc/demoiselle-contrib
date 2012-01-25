1bb8744fd65289530fedf79fe0d065cc84de3c01
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-01-24 22:20:52 -0300

    implementação do módulo LocalAuthenticator que realiza autenticação através da validação da senha na tabela SecurityUser utilizando hash SHA512.

--- demoiselle-fuselage/src/main/java/br/gov/frameworkdemoiselle/fuselage/authenticators/LocalAuthenticator.java
+++ demoiselle-fuselage/src/main/java/br/gov/frameworkdemoiselle/fuselage/authenticators/LocalAuthenticator.java
@@ -1,22 +1,53 @@
 package br.gov.frameworkdemoiselle.fuselage.authenticators;
 
-public class LocalAuthenticator implements AuthenticatorModule {
+import javax.inject.Inject;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import br.gov.frameworkdemoiselle.fuselage.business.UserBC;
+import br.gov.frameworkdemoiselle.fuselage.domain.SecurityUser;
+import br.gov.frameworkdemoiselle.util.Strings;
+
+public class LocalAuthenticator extends AbstractAuthenticatorModule<LocalAuthenticator> {
+
+	@Inject
+	private UserBC userBC;
 
 	private AuthenticatorResults results = new AuthenticatorResults();
 
-	public boolean authenticate(String username, String password) {
-		results = new AuthenticatorResults();
-
-/*		if (authenticated)
-			results.setAuthenticatorModuleName(getClass().getSimpleName());
-*/		
-		
-		return false;
-	}
-
-	@Override
 	public AuthenticatorResults getResults() {
 		return results;
+	}
+
+	public boolean authenticate(String username, String password) {
+		results = new AuthenticatorResults();
+		results.setAuthenticatorModuleName(getModuleName());
+		results.setLoggedIn(login(username, password));
+		if (results.isLoggedIn())
+			getLogger().info(getBundle().getString("fuselage.authenticators.login.success", username, getModuleName()));
+		else
+			getLogger().info(getBundle().getString("fuselage.authenticators.login.failed", username, getModuleName()));
+		if (results.isUserUnavailable())
+			getLogger().info(getBundle().getString("fuselage.authenticators.login.unavailable", username, getModuleName()));
+		return results.isLoggedIn();
+	}
+
+	private boolean login(String username, String password) {
+		if (Strings.isBlank(username) || Strings.isBlank(password))
+			return false;
+
+		SecurityUser securityUser = userBC.loadByLogin(username);
+		results.setSecurityUser(securityUser);
+
+		if (securityUser.getId() != null && !securityUser.isEnabled()) {
+			results.setUserUnavailable(true);
+			return false;
+		}
+
+		if (DigestUtils.sha512Hex(password).equals(securityUser.getPassword())) {
+			return true;
+		}
+		return false;
 	}
 
 }
