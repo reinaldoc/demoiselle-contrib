3b009bbb8e45a672063d1d4099a10d36060d4f01
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-02-04 18:27:05 -0300

    implementação de métodos para acesso ao QueryConfig, classe que transporta parâmetros para a consulta a ser realizada para a camada de persistencia.

---
+++ demoiselle-core-contrib/src/main/java/br/gov/frameworkdemoiselle/template/contrib/DelegateCrud.java
@@ -0,0 +1,125 @@
+/*
+ * Demoiselle Framework
+ * Copyright (C) 2010 SERPRO
+ * ----------------------------------------------------------------------------
+ * This file is part of Demoiselle Framework.
+ * 
+ * Demoiselle Framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not,  see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * Este arquivo é parte do Framework Demoiselle.
+ * 
+ * O Framework Demoiselle é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * do Software Livre (FSF).
+ * 
+ * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
+ * para maiores detalhes.
+ * 
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
+package br.gov.frameworkdemoiselle.template.contrib;
+
+import java.util.List;
+import java.util.ListIterator;
+
+import javax.inject.Inject;
+
+import br.gov.frameworkdemoiselle.query.contrib.QueryConfig;
+import br.gov.frameworkdemoiselle.query.contrib.QueryContext;
+import br.gov.frameworkdemoiselle.template.Crud;
+import br.gov.frameworkdemoiselle.transaction.Transactional;
+import br.gov.frameworkdemoiselle.util.Beans;
+import br.gov.frameworkdemoiselle.util.Reflections;
+
+public class DelegateCrud<T, I, C extends Crud<T, I>> implements Crud<T, I> {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private QueryContext queryContext;
+
+	private Class<C> delegateClass;
+
+	private C delegate;
+
+	private Class<T> beanClass;
+
+	@Override
+	@Transactional
+	public void delete(final I id) {
+		this.getDelegate().delete(id);
+	}
+
+	@Transactional
+	public void delete(final List<I> idList) {
+		ListIterator<I> iter = idList.listIterator();
+		while (iter.hasNext()) {
+			this.delete(iter.next());
+		}
+	}
+
+	@Override
+	public List<T> findAll() {
+		return getDelegate().findAll();
+	}
+
+	@Override
+	@Transactional
+	public void insert(final T bean) {
+		getDelegate().insert(bean);
+	}
+
+	@Override
+	public T load(final I id) {
+		return getDelegate().load(id);
+	}
+
+	@Override
+	@Transactional
+	public void update(final T bean) {
+		getDelegate().update(bean);
+	}
+
+	protected C getDelegate() {
+		if (this.delegate == null) {
+			this.delegate = Beans.getReference(getDelegateClass());
+		}
+		return this.delegate;
+	}
+
+	protected Class<C> getDelegateClass() {
+		if (this.delegateClass == null) {
+			this.delegateClass = Reflections.getGenericTypeArgument(this.getClass(), 2);
+		}
+		return this.delegateClass;
+	}
+
+	protected Class<T> getBeanClass() {
+		if (this.beanClass == null) {
+			this.beanClass = Reflections.getGenericTypeArgument(this.getClass(), 0);
+		}
+		return this.beanClass;
+	}
+
+	protected QueryConfig<T> getQueryConfig() {
+		return queryContext.getQueryConfig(getBeanClass(), true);
+	}
+
+}
