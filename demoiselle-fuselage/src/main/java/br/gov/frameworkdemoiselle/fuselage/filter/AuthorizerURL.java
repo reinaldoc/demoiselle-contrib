8dbde1c5b6daf285f61fdb05f3a1c7bbd93cdc5d
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-02-05 19:24:27 -0300

    refatoração e definição do dispatcher pois o webfilter não estava sendo executado para mapeamento do prettyfaces

--- demoiselle-fuselage/src/main/java/br/gov/frameworkdemoiselle/fuselage/filter/AuthorizerURL.java
+++ demoiselle-fuselage/src/main/java/br/gov/frameworkdemoiselle/fuselage/filter/AuthorizerURL.java
@@ -3,6 +3,7 @@ package br.gov.frameworkdemoiselle.fuselage.filter;
 import java.io.IOException;
 
 import javax.inject.Inject;
+import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -21,7 +22,7 @@ import br.gov.frameworkdemoiselle.internal.configuration.JsfSecurityConfig;
 import br.gov.frameworkdemoiselle.internal.producer.LoggerProducer;
 import br.gov.frameworkdemoiselle.security.SecurityContext;
 
-@WebFilter(urlPatterns = { "/*" })
+@WebFilter(urlPatterns = { "/*" }, dispatcherTypes = { DispatcherType.FORWARD, DispatcherType.REQUEST })
 public class AuthorizerURL implements Filter {
 
 	private Logger logger = LoggerProducer.create(AuthorizerURL.class);
@@ -52,32 +53,36 @@ public class AuthorizerURL implements Filter {
 		this.request = (HttpServletRequest) request;
 		String url = this.request.getRequestURI().replaceAll("^/.+?/", "/");
 
+		if (isPublicURL(url) || hasPermission(url))
+			chain.doFilter(request, response);
+		else
+			redirect(response, getContext() + config.getLoginPage());
+	}
+
+	private boolean isPublicURL(String url) {
 		if (publicResources.hasPermission("public_url", url, Comparison.EQUALS) || url.equals(config.getLoginPage())) {
-			info("permitted by public resource", url);
-			chain.doFilter(request, response);
-			return;
+			info("permitted by public_url", url);
+			return true;
+		} else if (publicResources.hasPermission("public_url_startswith", url, Comparison.STARTSWITH)) {
+			info("permitted by public_url_startswith", url);
+			return true;
 		}
+		return false;
+	}
 
-		if (publicResources.hasPermission("public_url_startswith", url, Comparison.STARTSWITH)) {
-			info("permitted by public resource", url);
-			chain.doFilter(request, response);
-			return;
-		}
-
+	private boolean hasPermission(String url) {
 		if (!securityContext.isLoggedIn()) {
 			info("denied by not logged in, redirect to login page", url);
-			redirect(response, getContext() + config.getLoginPage());
-			chain.doFilter(request, response);
-			return;
+			return false;
 		}
 
 		if (securityContext.hasPermission("private_url", url)) {
 			info("permitted by resource", url);
+			return true;
 		} else {
 			info("denied by resource, redirect to welcome page", url);
-			redirect(response, getContext() + config.getLoginPage());
+			return false;
 		}
-		chain.doFilter(request, response);
 	}
 
 	private void redirect(ServletResponse response, String url) throws IOException {
