e72a2994b85e072e031fa48437b1d0446acf7d0f
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-02-06 18:01:44 -0300

    limpar o QueryConfig para obter todos os SecurituResources

--- demoiselle-fuselage/src/main/java/br/gov/frameworkdemoiselle/fuselage/business/RoleBC.java
+++ demoiselle-fuselage/src/main/java/br/gov/frameworkdemoiselle/fuselage/business/RoleBC.java
@@ -7,7 +7,7 @@ import javax.inject.Inject;
 import br.gov.frameworkdemoiselle.fuselage.domain.SecurityResource;
 import br.gov.frameworkdemoiselle.fuselage.domain.SecurityRole;
 import br.gov.frameworkdemoiselle.fuselage.persistence.RoleDAO;
-import br.gov.frameworkdemoiselle.template.DelegateCrud;
+import br.gov.frameworkdemoiselle.template.contrib.DelegateCrud;
 
 public class RoleBC extends DelegateCrud<SecurityRole, Long, RoleDAO> {
 	private static final long serialVersionUID = 1L;
@@ -16,6 +16,7 @@ public class RoleBC extends DelegateCrud<SecurityRole, Long, RoleDAO> {
 	private ResourceBC resourceBC;
 
 	public List<SecurityResource> getResources() {
+		getQueryConfig(SecurityResource.class).getFilter().clear();
 		return resourceBC.findAll();
 	}
 
