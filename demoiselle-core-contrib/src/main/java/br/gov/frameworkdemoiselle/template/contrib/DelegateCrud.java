4ba57cb9dbebd62093f96c4d35f604023aff6518
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-02-12 21:23:19 -0300

    integração do DelegateCrud (classe abstrata da camada de negócio) com o componente MenuContext;

--- demoiselle-core-contrib/src/main/java/br/gov/frameworkdemoiselle/template/contrib/DelegateCrud.java
+++ demoiselle-core-contrib/src/main/java/br/gov/frameworkdemoiselle/template/contrib/DelegateCrud.java
@@ -47,6 +47,7 @@ import br.gov.frameworkdemoiselle.template.Crud;
 import br.gov.frameworkdemoiselle.transaction.Transactional;
 import br.gov.frameworkdemoiselle.util.Beans;
 import br.gov.frameworkdemoiselle.util.Reflections;
+import br.gov.frameworkdemoiselle.util.core.MenuContext;
 
 public class DelegateCrud<T, I, C extends Crud<T, I>> implements Crud<T, I> {
 
@@ -54,6 +55,9 @@ public class DelegateCrud<T, I, C extends Crud<T, I>> implements Crud<T, I> {
 
 	@Inject
 	private QueryContext queryContext;
+
+	@Inject
+	private MenuContext menuContext;
 
 	private Class<C> delegateClass;
 
@@ -124,6 +128,18 @@ public class DelegateCrud<T, I, C extends Crud<T, I>> implements Crud<T, I> {
 
 	protected QueryConfig<?> getQueryConfig(Class<?> clazz) {
 		return queryContext.getQueryConfig(clazz, true);
+	}
+
+	protected MenuContext getMenuContext() {
+		return menuContext;
+	}
+
+	protected String getSelectedMenu() {
+		return menuContext.getSelected(getBeanClass().getSimpleName());
+	}
+
+	protected void selectMenu(String itemName) {
+		menuContext.select(getBeanClass().getSimpleName(), itemName);
 	}
 
 }
