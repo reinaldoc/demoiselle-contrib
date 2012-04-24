cea7bce886f9a58d72de8cf24a1bdae11c2fa3c9
Reinaldo Gil Lima de Carvalho <reinaldoc@gmail.com>
2012-04-24 13:17:46 -0300

    ativação do CDI

--- demoiselle-jsf-contrib/src/main/java/br/gov/frameworkdemoiselle/util/core/MenuContextFaces.java
+++ demoiselle-jsf-contrib/src/main/java/br/gov/frameworkdemoiselle/util/core/MenuContextFaces.java
@@ -2,12 +2,13 @@ package br.gov.frameworkdemoiselle.util.core;
 
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Alternative;
 import javax.inject.Inject;
 
 import br.gov.frameworkdemoiselle.util.Parameter;
-import br.gov.frameworkdemoiselle.util.core.MenuContext;
 
 @SessionScoped
+@Alternative
 public class MenuContextFaces extends MenuContext {
 
 	private static final long serialVersionUID = 1L;
